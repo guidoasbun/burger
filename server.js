4836309da4244634691c94bfe724a6f0c40711e2
guidoasbun <gdrodrigo8@gmail.com>
2020-03-11 22:06:20 -0700

    setup config

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+// require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const { join } = require('path')
 const app = express()
+const Burger = require('./models/burger.js')
 
-app.set('views', join(__dirname + 'views')
-app.set('view engine', 'jsx')
+
+// middleware
 app.engine('jsx', require('express-react-views').createEngine())
+app.set('views', join(__dirname + 'views'))
+app.set('view engine', 'jsx')
 
-app.use(require('./routes'))
+
+// app.use(require('./routes'))
 
 app.listen(3000)
---
+++ views/home.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Html = require('./layouts/default.jsx')
+const Home = props => {
+  return(
+    <Html>
+
+    </Html>
+
+
+  )
+}
+
+
+
+module.exports = Home
