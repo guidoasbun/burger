2b939b2bacf523ed4e4720deb0df3978db9e938e
guidoasbun <gdrodrigo8@gmail.com>
2020-03-13 10:40:48 -0700

    changes

--- config/index.js
+++ config/index.js
@@ -13,6 +13,6 @@
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.NODE_ENV === 'prodution' ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
+const sequelize = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
 
 module.exports = sequelize
